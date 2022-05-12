cb52b79e431de2f298daf016e7980055d371293f
michelrubens <michelrube@gmail.com>
2022-05-12 00:39:42 -0300

    Criação da tela de cadastro de promoção
    Criação do componente CadastroDePromocao ainda sem persistência dos dados.

--- src/App.vue
+++ src/App.vue
@@ -6,51 +6,34 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <a class="navbar-brand" href="#">
+          <img src="./assets/logo.png" alt="" width="50" height="50">
+        </a>
+        <h2>DYNAMICS</h2>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <input class="form-control form-control-sm" 
+        type="text" 
+        placeholder="Pesquisar" 
+        aria-label="pesquisar">
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <CadastroDeProduto/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import CadastroDeProduto from './components/CadastroDeProduto';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    CadastroDeProduto,
   },
 
   data: () => ({
---
+++ src/components/CadastroDeProduto.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container-md">
+    <!-- Content here -->
+    <h3>Cadastro de Produto</h3>
+
+    <form>
+      <div class="mb-3">
+        <label for="produto" class="form-label">Produto</label>
+        <input
+          type="text"
+          class="form-control"
+          id="produto"
+          placeholder="Insira o nome do produto"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="descricao" class="form-label">Descrição</label>
+        <textarea
+          class="form-control"
+          id="descricao"
+          rows="3"
+          placeholder="Descrição do produto"
+        ></textarea>
+      </div>
+      <div class="mb-3">
+        <label for="categoria" class="form-label">Categoria</label>
+        <select class="form-select" aria-label="Categoria de produto">
+          <option selected>(Selecione uma categoria)</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
+      <div class="mb-3">
+        <label for="preco" class="form-label">Preço</label>
+        <input
+          type="number"
+          class="form-control"
+          id="preco"
+          placeholder="R$"
+        />
+      </div>
+      <div><input class="btn btn-primary" type="submit" value="Cadastrar"></div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CadastroDeProduto",
+};
+</script>
