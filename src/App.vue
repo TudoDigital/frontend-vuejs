6ebfcf67a3d49aa8570b4b8ccbaefe4c592c9298
Rafael Trevizoli <rafael.trevizoli@autaza.com>
2022-05-28 19:37:54 -0300

    Navy as component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <a class="navbar-brand" href="#">
-          <img src="./assets/logo.png" alt="" width="50" height="50">
-        </a>
-        <h2>DYNAMICS</h2>
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <input class="form-control form-control-sm" 
-        type="text" 
-        placeholder="Pesquisar" 
-        aria-label="pesquisar">
-    </v-app-bar>
-
+    <MenuDynamics/>
     <v-main>
       <p>
           <router-link to="/">Consulta</router-link>
@@ -34,6 +15,7 @@
 </template>
 
 <script>
+import MenuDynamics from './components/MenuDynamics'
 import HttpAxios from './components/HttpAxios'
 import HttpFetch from './components/HttpFetch'
 
@@ -41,6 +23,7 @@ export default {
   name: 'App',
 
   components: {
+    MenuDynamics,
     HttpAxios,
     HttpFetch
   },
