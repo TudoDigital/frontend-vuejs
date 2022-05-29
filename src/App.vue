7cacdff8d28dc27809526e31e94c1ee37f6f4c6b
Rafael Trevizoli <rafael.trevizoli@autaza.com>
2022-05-28 21:52:36 -0300

    System navigation

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,58 @@
 <template>
   <v-app>
-    <MenuDynamics/>
+    <v-navigation-drawer app dark>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h5 mt-2">
+            <v-row>
+              <v-col md=4>
+                <img src="./assets/logo.png" class="" width="50" height="50">
+              </v-col>
+              <v-col class="mt-4 ml-0 pl-0">
+                DYNAMICS
+              </v-col>
+            </v-row>
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          
+          <v-list-item to="/">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Consulta</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item to="/produto">
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Cadastro de Produtos</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
-      <p>
-          <router-link to="/">Consulta</router-link>
-          <br>
-          <router-link to="/produto">Cadastro de Produtos</router-link>
-      </p>
-      <router-view></router-view>
-      <HttpAxios />
-      <HttpFetch />
+      <v-container fluid>
+        <router-view></router-view>
+        <HttpAxios />
+        <HttpFetch />
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import MenuDynamics from './components/MenuDynamics'
 import HttpAxios from './components/HttpAxios'
 import HttpFetch from './components/HttpFetch'
 
@@ -23,13 +60,12 @@ export default {
   name: 'App',
 
   components: {
-    MenuDynamics,
     HttpAxios,
     HttpFetch
   },
 
   data: () => ({
-    //
+      group: null,
   }),
 };
 </script>
